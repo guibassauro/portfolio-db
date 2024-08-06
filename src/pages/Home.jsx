56688accaae0923baa8a165ac96892083a589893
Samara Bastos <samarabastos.dev@gmail.com>
2024-08-06 16:41:08 -0300

    Ajuste na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
+import styled from "styled-components";
+import Apresentacao from "../components/apresentacao";
+import Foto from '../assets/foto.jpg'
+import Sobre from "../components/sobre";
+import Habilidades from "../components/habilidades";
+
+const ContainerHome = styled.div`
+    min-height: 96vh;
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 157px 100px 20px 100px;
+    width: 100%;
+    gap: 10px;
+`
+
+const Imagem = styled.img`
+    object-fit: cover;
+    Width: 567px;
+    Height: 292px;
+`
+
 
 function Home(){
     return(
-        <div>
-            Home
-        </div>
+        <ContainerHome>
+            <Apresentacao />
+            <Imagem src={Foto} alt="foto" />
+            <Sobre />
+            <Habilidades />
+        </ContainerHome>
     )
 }
 
