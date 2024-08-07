ec089797970802e5c32f5d3019c96f58128fcde6
Samara Bastos <samarabastos.dev@gmail.com>
2024-08-07 17:10:29 -0300

    Ajuste no texto sobre mim

--- src/components/sobre/index.jsx
+++ src/components/sobre/index.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const ContainerSobre = styled.div`
     min-height: 48vh;
-    max-width: 460px;
+    max-width: 660px;
+    text-align: justify;
 `
 
 const Texto = styled.h1`
@@ -19,11 +20,11 @@ function Sobre(){
                 Sobre mim
             </Texto>
             <Texto TamFonte={'16px'} Weight={'300'}>
-                Sou dev há pouco mais de um ano e já me aventurei em diversas tecnologias web, 
-                incluindo JavaScript, jQuery, HTML, CSS e PHP. Atualmente, estou me especializando em uma nova area, 
-                trabalhando com  Java , Spring Boot, JPA, APIs REST, React, Chakra UI, entre outras. <br />
-               
-                Meu objetivo de carreira é aprender cada vez mais para me tornar uma engenheira de software.
+                Sou desenvolvedora há pouco mais de um ano e amo o que faço! Mas nem sempre foi assim. Minha jornada começou em 2019, quando eu estava finalizando o ensino médio. Minha mãe me mostrou uma publicação de um novo curso no Instituto Federal para 'consertar computadores' e, curiosa, me inscrevi. Era um curso de programação para desenvolver sistemas. Me arrependi amargamente, pois eu não conseguia entender nada, não conseguia estágio, enfim, foi um caos e eu desisti. Terminei o ensino médio e consegui uma bolsa para cursar marketing. Enquanto cursava a graduação, minha mente ainda estava na TI. Eu não estava aceitando muito bem que tinha desistido de uma profissão por ter achado difícil, e, para piorar, eu não estava me identificando nem um pouco com marketing.
+                <br />
+                Finalizei a faculdade, mas, ao invés de tentar emprego na área de marketing, comecei a estudar programação sozinha. Achei um ótimo curso de algoritmos com o Guanabara e me empenhei. Finalmente peguei o jeito; só me faltava fazer uma boa base na lógica e ser mais proativa com meus estudos.
+                <br />
+                Voltei e concluí o meu curso técnico. Hoje, curso a graduação em Engenharia de Software e, em agosto de 2024, completo dois anos trabalhando na área.
             </Texto>
             
         </ContainerSobre>
